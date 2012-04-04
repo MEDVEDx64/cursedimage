9da295a9f946314b2fa5975933849e967d915392
Andrey Bobkov <medvedx64@gmail.com>
2012-04-04 21:40:30 +0400

    redesigned color system

---
+++ cursedimage_color.cpp
@@ -0,0 +1,90 @@
+#include "cursedimage_color.h"
+
+bool cursed_init_color()
+{
+#ifdef __WIN32__
+	return false;
+#endif
+#ifndef MONO
+
+	if(!start_color()) return false;
+
+	/* Initializing color pairs */
+// *K
+	init_pair(COL_KK, COLOR_BLACK,	COLOR_BLACK);
+	init_pair(COL_RK, COLOR_RED,	COLOR_BLACK);
+	init_pair(COL_YK, COLOR_YELLOW,	COLOR_BLACK);
+	init_pair(COL_GK, COLOR_GREEN,	COLOR_BLACK);
+	init_pair(COL_CK, COLOR_CYAN,	COLOR_BLACK);
+	init_pair(COL_BK, COLOR_BLUE,	COLOR_BLACK);
+	init_pair(COL_MK, COLOR_MAGENTA,COLOR_BLACK);
+	init_pair(COL_WK, COLOR_WHITE,	COLOR_BLACK);
+// *R
+	init_pair(COL_KR, COLOR_BLACK,	COLOR_RED);
+	init_pair(COL_RR, COLOR_RED,	COLOR_RED);
+	init_pair(COL_YR, COLOR_YELLOW,	COLOR_RED);
+	init_pair(COL_GR, COLOR_GREEN,	COLOR_RED);
+	init_pair(COL_CR, COLOR_CYAN,	COLOR_RED);
+	init_pair(COL_BR, COLOR_BLUE,	COLOR_RED);
+	init_pair(COL_MR, COLOR_MAGENTA,COLOR_RED);
+	init_pair(COL_WR, COLOR_WHITE,	COLOR_RED);
+// *Y
+	init_pair(COL_KY, COLOR_BLACK,	COLOR_YELLOW);
+	init_pair(COL_RY, COLOR_RED,	COLOR_YELLOW);
+	init_pair(COL_YY, COLOR_YELLOW,	COLOR_YELLOW);
+	init_pair(COL_GY, COLOR_GREEN,	COLOR_YELLOW);
+	init_pair(COL_CY, COLOR_CYAN,	COLOR_YELLOW);
+	init_pair(COL_BY, COLOR_BLUE,	COLOR_YELLOW);
+	init_pair(COL_MY, COLOR_MAGENTA,COLOR_YELLOW);
+	init_pair(COL_WY, COLOR_WHITE,	COLOR_YELLOW);
+// *G
+	init_pair(COL_KG, COLOR_BLACK,	COLOR_GREEN);
+	init_pair(COL_RG, COLOR_RED,	COLOR_GREEN);
+	init_pair(COL_YG, COLOR_YELLOW,	COLOR_GREEN);
+	init_pair(COL_GG, COLOR_GREEN,	COLOR_GREEN);
+	init_pair(COL_CG, COLOR_CYAN,	COLOR_GREEN);
+	init_pair(COL_BG, COLOR_BLUE,	COLOR_GREEN);
+	init_pair(COL_MG, COLOR_MAGENTA,COLOR_GREEN);
+	init_pair(COL_WG, COLOR_WHITE,	COLOR_GREEN);
+// *C
+	init_pair(COL_KC, COLOR_BLACK,	COLOR_CYAN);
+	init_pair(COL_RC, COLOR_RED,	COLOR_CYAN);
+	init_pair(COL_YC, COLOR_YELLOW,	COLOR_CYAN);
+	init_pair(COL_GC, COLOR_GREEN,	COLOR_CYAN);
+	init_pair(COL_CC, COLOR_CYAN,	COLOR_CYAN);
+	init_pair(COL_BC, COLOR_BLUE,	COLOR_CYAN);
+	init_pair(COL_MC, COLOR_MAGENTA,COLOR_CYAN);
+	init_pair(COL_WC, COLOR_WHITE,	COLOR_CYAN);
+// *B
+	init_pair(COL_KB, COLOR_BLACK,	COLOR_BLUE);
+	init_pair(COL_RB, COLOR_RED,	COLOR_BLUE);
+	init_pair(COL_YB, COLOR_YELLOW,	COLOR_BLUE);
+	init_pair(COL_GB, COLOR_GREEN,	COLOR_BLUE);
+	init_pair(COL_CB, COLOR_CYAN,	COLOR_BLUE);
+	init_pair(COL_BB, COLOR_BLUE,	COLOR_BLUE);
+	init_pair(COL_MB, COLOR_MAGENTA,COLOR_BLUE);
+	init_pair(COL_WB, COLOR_WHITE,	COLOR_BLUE);
+// *M
+	init_pair(COL_KM, COLOR_BLACK,	COLOR_MAGENTA);
+	init_pair(COL_RM, COLOR_RED,	COLOR_MAGENTA);
+	init_pair(COL_YM, COLOR_YELLOW,	COLOR_MAGENTA);
+	init_pair(COL_GM, COLOR_GREEN,	COLOR_MAGENTA);
+	init_pair(COL_CM, COLOR_CYAN,	COLOR_MAGENTA);
+	init_pair(COL_BM, COLOR_BLUE,	COLOR_MAGENTA);
+	init_pair(COL_MM, COLOR_MAGENTA,COLOR_MAGENTA);
+	init_pair(COL_WM, COLOR_WHITE,	COLOR_MAGENTA);
+// *W
+	init_pair(COL_KW, COLOR_BLACK,	COLOR_WHITE);
+	init_pair(COL_RW, COLOR_RED,	COLOR_WHITE);
+	init_pair(COL_YW, COLOR_YELLOW,	COLOR_WHITE);
+	init_pair(COL_GW, COLOR_GREEN,	COLOR_WHITE);
+	init_pair(COL_CW, COLOR_CYAN,	COLOR_WHITE);
+	init_pair(COL_BW, COLOR_BLUE,	COLOR_WHITE);
+	init_pair(COL_MW, COLOR_MAGENTA,COLOR_WHITE);
+	init_pair(COL_WW, COLOR_WHITE,	COLOR_WHITE);
+
+	return true;
+#else
+	return false;
+#endif
+}
---
+++ cursedimage_color.h
@@ -0,0 +1,100 @@
+/*
+ * ============================================
+ *
+ * Cursed Color
+ *
+ * How it stores the color:
+ *	bits 1-2 - dummy;
+ *	bitt 3-5 - foreground bool RGB color;
+ *	bits 6-8 - background bool RGB color.
+ *		Only one byte is enough
+ *
+ * MEDVEDx64, 2012.04.04
+ *
+ * ============================================
+ */
+
+#ifndef __WIN32__
+#	include <ncurses.h>
+#endif
+
+/* Color definitions */
+
+enum
+{
+// *K
+	COL_KK = 0b000000,
+	COL_RK = 0b100000,
+	COL_YK = 0b110000,
+	COL_GK = 0b010000,
+	COL_CK = 0b011000,
+	COL_BK = 0b001000,
+	COL_MK = 0b101000,
+	COL_WK = 0b111000,
+// *R
+	COL_KR = 0b000100,
+	COL_RR = 0b100100,
+	COL_YR = 0b110100,
+	COL_GR = 0b010100,
+	COL_CR = 0b011100,
+	COL_BR = 0b001100,
+	COL_MR = 0b101100,
+	COL_WR = 0b111100,
+// *Y
+	COL_KY = 0b000110,
+	COL_RY = 0b100110,
+	COL_YY = 0b110110,
+	COL_GY = 0b010110,
+	COL_CY = 0b011110,
+	COL_BY = 0b001110,
+	COL_MY = 0b101110,
+	COL_WY = 0b111110,
+// *G
+	COL_KG = 0b000010,
+	COL_RG = 0b100010,
+	COL_YG = 0b110010,
+	COL_GG = 0b010010,
+	COL_CG = 0b011010,
+	COL_BG = 0b001010,
+	COL_MG = 0b101010,
+	COL_WG = 0b111010,
+// *C
+	COL_KC = 0b000011,
+	COL_RC = 0b100011,
+	COL_YC = 0b110011,
+	COL_GC = 0b010011,
+	COL_CC = 0b011011,
+	COL_BC = 0b001011,
+	COL_MC = 0b101011,
+	COL_WC = 0b111011,
+// *B
+	COL_KB = 0b000001,
+	COL_RB = 0b100001,
+	COL_YB = 0b110001,
+	COL_GB = 0b010001,
+	COL_CB = 0b011001,
+	COL_BB = 0b001001,
+	COL_MB = 0b101001,
+	COL_WB = 0b111001,
+// *M
+	COL_KM = 0b000101,
+	COL_RM = 0b100101,
+	COL_YM = 0b110101,
+	COL_GM = 0b010101,
+	COL_CM = 0b011101,
+	COL_BM = 0b001101,
+	COL_MM = 0b101101,
+	COL_WM = 0b111101,
+// *W
+	COL_KW = 0b000111,
+	COL_RW = 0b100111,
+	COL_YW = 0b110111,
+	COL_GW = 0b010111,
+	COL_CW = 0b011111,
+	COL_BW = 0b001111,
+	COL_MW = 0b101111,
+	COL_WW = 0b111111
+
+};
+
+bool cursed_init_color();
