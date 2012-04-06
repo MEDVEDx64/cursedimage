7a6b70bd83c382d8c81a24f64b15d7c0028d0d73
Andrey Bobkov <medvedx64@gmail.com>
2012-04-06 17:19:48 +0400

    cursed_imgsave is now returns bool

--- cursedimage_file.h
+++ cursedimage_file.h
@@ -1,8 +1,8 @@
 // This file is only needed to operate
-//      with the Cursed image format
+//      with the Cursed image file format
 
     /// Part of die Cursed Library
-     // MEDVEDx64 © Feb., March 2012
+     // MEDVEDx64 © Feb.-April 2012
 
 #ifndef CURSEDIMAGE_FILE_H_INCLUDED
 #define CURSEDIMAGE_FILE_H_INCLUDED
@@ -32,16 +32,8 @@ struct TCursedImage
     TCursedChar *image;
 };
 
-enum
-{
-    COL_WK, COL_RK, COL_YK, COL_GK, COL_BK, COL_CK, COL_MK,
-    COL_KW, COL_KR, COL_KY, COL_KG, COL_KB, COL_KC, COL_KM,
-    COL_WW, COL_WR, COL_WY, COL_WG, COL_WB, COL_WC, COL_WM,
-    COL_KK = 0xFF
-};
-
 TCursedImage    cursed_blank(unsigned char w, unsigned char h);
 TCursedImage    cursed_imgload(const char* fname);
-void            cursed_imgsave(const char* fname, TCursedImage img);
+bool            cursed_imgsave(const char* fname, TCursedImage img);
 
 #endif // CURSEDIMAGE_FILE_H_INCLUDED
